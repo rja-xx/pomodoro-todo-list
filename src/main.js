0db48f5d58baeab4344b0df4b34ebe0e63fce383
Roger Jakobsen <roger.jakobsen@embriq.no>
2019-09-23 12:37:59 +0200

    added vuex and store

--- src/main.js
+++ src/main.js
@@ -15,9 +15,34 @@ const store = new Vuex.Store({
     },
     mutations:{
 
+          setCognitoSession(state, cognitoSession) {
+            state.cognito.session = cognitoSession;
+          },
+          setCognitoAuth(state, cognitoAuth) {
+            state.cognito.auth = cognitoAuth;
+          },
     }
 
 });
+
+
+
+setInterval(() => {
+  var session = store.state.cognito.session;
+
+  if (session && session.idToken && session.refreshToken) {
+    var expires = session.idToken.payload.exp;
+    var now = new Date().getTime() / 1000;
+
+    // Refresh session if ID token expires in less than 30 minutes.
+    if (expires - now < 30 * 60) {
+      console.log(
+        "Cognito token expires in " + (expires - now) + " seconds. Refreshing."
+      );
+      store.state.cognito.auth.refreshSession(session.refreshToken.getToken());
+    }
+  }
+}, 5 * 60000); // check every 5 minutes
 
 new Vue({
   render: h => h(App),
